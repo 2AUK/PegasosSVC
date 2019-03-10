cb3248abf5b1b64f0928a96481d4cf5bf9fc9e19
abdullah <gxb14174@uni.strath.ac.uk>
2019-03-10 00:45:54 +0000

    Most of the code is implemented. Now need to fix for maths

--- Pegasos.py
+++ Pegasos.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 class SVC(object):
@@ -11,29 +13,31 @@ class SVC(object):
         self.C = C
         self.w = np.ndarray
         self.X_train = np.ndarray
-        self.X_test = np.ndarray
-        self.y_test = np.ndarray
-        self.y_pred = np.ndarray
+        self.y_train = np.ndarray
 
-    def read_file(input_file):
-        with open(input_file, 'r') as ifile:
-            pass
+    def predict(self, X_test):
+        """Computes f(x) = wx (ignoring bias term for now) and then use a decision function (sign(), really)"""
+        return np.asarray([np.sign(y) for y in self.w.dot(X_test.T)])
             
-
-    def predict():
-        """Computes f(x) = wx (ignoring bias term for now)"""
-        pass
-
-    def optimise_weights():
+    def fit(self, T):
         """performs SGD for computing minimised w as described in Pegasos paper"""
-        pass
-
-    def preprocess_data():
-        """splitting data in to testing and training tests"""
-        pass
-
-
-
+        m, n_features = self.X_train.shape[0], self.X_train.shape[1]
+        self.w = np.zeros(n_features)
+        for i in range(T):
+            eta = 1 / self.gamma * (i+1)
+            j = np.random.choice(m, 1)[0]
+            x, y = self.X_train[j], self.y_train[j]
+            if y*(self.w.dot(x)) < 1:
+                self.w = (1 - eta*self.gamma)*self.w + eta*y*x
+            else:
+                self.w = (1 - eta*self.gamma)*self.w
+                
 
 if __name__ == "__main__":
     svc = SVC(0.001, 100)
+    all_data = datasets.load_breast_cancer()
+    X_train, X_test, y_train, y_test = train_test_split(all_data.data, all_data.target, test_size=0.3, random_state=109)
+    svc.X_train = X_train
+    svc.y_train = y_train
+    svc.fit(1000)
+    print(svc.predict(X_test))
