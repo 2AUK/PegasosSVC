7297da8974f4c76061cf8d946e9dc94764fa47ff
abdullah <gxb14174@uni.strath.ac.uk>
2019-03-12 00:26:46 +0000

    At least it's not arrays of only 1 or -1 anymore

--- Pegasos.py
+++ Pegasos.py
@@ -2,14 +2,14 @@ import numpy as np
 import csv
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn import svm
 
 
 class SVC(object):
 
-    def __init__(self, gamma, C):
+    def __init__(self, C):
         #Assume linear
         self.kernel = "linear"
-        self.gamma = gamma
         self.C = C
         self.w = np.ndarray
         self.X_train = np.ndarray
@@ -17,30 +17,33 @@ class SVC(object):
 
     def predict(self, X_test):
         """Computes f(x) = wx (ignoring bias term for now) and then use a decision function (sign(), really)"""
-        return np.asarray([np.sign(y) for y in self.w.dot(X_test.T)])
+        return np.sign(np.dot(self.w, X_test.T)).astype(int)
             
     def fit(self, T):
         """performs SGD for computing minimised w as described in Pegasos paper"""
         m, n_features = self.X_train.shape[0], self.X_train.shape[1]
-        self.w = np.zeros(n_features)
-        for i in range(1, T+1):
-            eta = 1. / self.gamma * i
-            j = np.random.choice(m, 1)[0]
+        print(m)
+        self.w = np.zeros(n_features, dtype=np.float64)
+        for i in range(1, m*100):
+            eta = 1. / self.C * float(i)
+            j = np.random.randint(m)
             x, y = self.X_train[j], self.y_train[j]
-            if y*(self.w.dot(x)) < 1:
-                self.w = (1 - eta*self.gamma)*self.w + eta*y*x
+            if y*(np.dot(self.w, x.T)) < 1:
+                self.w = (1. - 1/i)*self.w + eta*y*x
             else:
-                self.w = (1 - eta*self.gamma)*self.w
+                self.w = (1. - 1/i)*self.w
                 
 
 if __name__ == "__main__":
-    svc = SVC(1, 1)
+    svc = SVC(2)
     all_data = datasets.load_breast_cancer()
-    X_train, X_test, y_train, y_test = train_test_split(all_data.data, all_data.target, test_size=0.3, random_state=109)
+    X_train, X_test, y_train, y_test = train_test_split(all_data.data, all_data.target, test_size=0.3)
     svc.X_train = X_train
     y_train[y_train == 0] = -1
     y_test[y_test == 0] = -1
     svc.y_train = y_train
-    svc.fit(10)
+    #clf = svm.SVC(kernel='linear', max_iter=1000, C=0.5)
+    #clf.fit(X_train, y_train)
+    svc.fit(1000000)
     print(svc.predict(X_test))
     print(y_test)
